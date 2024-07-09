9b1a4ab64c1dac14c1cc2d2b03a3d6add533818a
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-07-09 22:37:30 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,15 @@ class BasicAuth(Auth):
     """
     Basic auth class
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extracting base64 authorization header
+        """
+        if authorization_header is None or\
+            not isinstance(authorization_header, str) or\
+                not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
+    
+
