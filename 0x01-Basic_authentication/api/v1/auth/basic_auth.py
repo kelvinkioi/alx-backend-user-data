7e06d66f001b0a05183cd6f43ab86fa12fc0ebac
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-07-09 22:56:00 +0300

    user credentials and user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,9 @@ Basic auth module
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import Tuple, TypeVar
+from models.user import User
+
 
 class BasicAuth(Auth):
     """
@@ -19,7 +22,6 @@ class BasicAuth(Auth):
                 not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
-    
 
     def decode_base64_authorization_header(self,
                                            base64_authorization_header:
@@ -33,4 +35,29 @@ class BasicAuth(Auth):
                 base64_authorization_header).decode('utf-8')
         except Exception:
             return None
-    
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> Tuple[str, str]:
+        """extract user credentials"""
+        if decoded_base64_authorization_header is None or\
+            not isinstance(decoded_base64_authorization_header, str) or\
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """user object from credentials"""
+        x = user_pwd is None or not isinstance(user_pwd, str)
+        if user_email is None or not isinstance(user_email, str) or x:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
