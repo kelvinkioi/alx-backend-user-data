5ce221db99eef00c319a8398f504a37ca1ebfca1
kelvinkioi <kelvinkioi.101k@gmail.com>
2024-07-18 20:59:44 +0300

    user authentication

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+"""
+auth file
+"""
+import bcrypt
+from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
+from typing import (
+    TypeVar,
+    Union
+)
+
+from db import DB
+from user import User
+
+U = TypeVar('U', bound=User)
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    method that takes in a password string arguments and returns bytes.
+    """
+    passwd = password.encode('utf-8')
+    return bcrypt.hashpw(passwd, bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    generates a uuid and return its string representation
+    """
+    return str(uuid4())
+
+
+class Auth:
+    """
+    authentication class to interact with
+    the authentication database
+    """
+
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        this takes mandatory email and password string arguments
+        and return a User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed = _hash_password(password)
+            usr = self._db.add_user(email, hashed)
+            return usr
+        raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        expects an email and a password as required arguments and return a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        user_password = user.hashed_password
+        passwd = password.encode("utf-8")
+        return bcrypt.checkpw(passwd, user_password)
+
+    def create_session(self, email: str) -> Union[None, str]:
+        """
+        takes an email string argument and returns the session ID as a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[None, U]:
+        """
+        takes a single session_id string argument and
+        returns the corresponding User or None
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        
+    def destroy_session(self, user_id: int) -> None:
+        """
+        method takes a single user_id integer argument and returns None
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
+        return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        takes an email string argument and returns a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        function to respond to the POST /reset_password route
+        the request is expected to contain form data with the "email" field
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+
+        hashed = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hashed, reset_token=None)
